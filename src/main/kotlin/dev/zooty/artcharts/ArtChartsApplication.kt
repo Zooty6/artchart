0244ac88cee6da4fe4c69cf888e8bd0802542c12
Zooty <zooty6@gmail.com>
2024-05-12 17:06:29 +0200

    init project

---
+++ src/main/kotlin/dev/zooty/artcharts/ArtChartsApplication.kt
@@ -0,0 +1,11 @@
+package dev.zooty.artcharts
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ArtChartsApplication
+
+fun main(args: Array<String>) {
+    runApplication<ArtChartsApplication>(*args)
+}
---
+++ src/test/kotlin/dev/zooty/artcharts/ArtChartsApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.zooty.artcharts
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ArtChartsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
