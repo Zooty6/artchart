3053aabee3472c4bbcb5e7264c83a3ca7b0c89b5
Zooty <zooty6@gmail.com>
2024-05-15 00:18:45 +0200

    doc

--- src/main/kotlin/dev/zooty/artcharts/controllers/api/HealthController.kt
+++ src/main/kotlin/dev/zooty/artcharts/controllers/api/HealthController.kt
@@ -1,12 +1,13 @@
 package dev.zooty.artcharts.controllers.api
 
+import org.springframework.http.MediaType
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class HealthController {
-    @GetMapping("/api/health")
+    @GetMapping("/api/health", produces = [MediaType.TEXT_PLAIN_VALUE])
     fun get(model: Model) : String {
         return "Hello :)"
     }
