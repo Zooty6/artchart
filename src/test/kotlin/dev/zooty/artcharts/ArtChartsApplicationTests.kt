e1e6cb7520698988beed47db10a1bc909df99d52
Zooty <zooty6@gmail.com>
2024-10-05 01:07:49 +0200

    minor refactor

--- src/test/kotlin/dev/zooty/artcharts/ArtChartsApplicationTests.kt
+++ src/test/kotlin/dev/zooty/artcharts/ArtChartsApplicationTests.kt
@@ -1,9 +1,7 @@
 package dev.zooty.artcharts
 
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
 class ArtChartsApplicationTests {
 
     @Test
